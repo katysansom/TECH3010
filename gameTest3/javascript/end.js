ba95a2639c158979919aed27385780d4cd941cb3
katysansom <72385894+katysansom@users.noreply.github.com>
2021-03-22 13:54:21 +0000

    Add files via upload

--- gameTest3/javascript/end.js
+++ gameTest3/javascript/end.js
@@ -1,65 +1,69 @@
 // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- var firebaseConfig = {
-   apiKey: "AIzaSyDuejSqZSWsXGYcO1qaILbyIpJJb5jvIUs",
-   authDomain: "smartzmultipage.firebaseapp.com",
-   databaseURL: "https://smartzmultipage-default-rtdb.firebaseio.com/",
-   projectId: "smartzmultipage",
-   storageBucket: "smartzmultipage.appspot.com",
-   messagingSenderId: "398309169877",
-   appId: "1:398309169877:web:daffc48ea2dbe54241a05c",
-   measurementId: "G-746DK30RKC"
- };
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- firebase.analytics();
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDuejSqZSWsXGYcO1qaILbyIpJJb5jvIUs",
+  authDomain: "smartzmultipage.firebaseapp.com",
+  databaseURL: "https://smartzmultipage-default-rtdb.firebaseio.com/",
+  projectId: "smartzmultipage",
+  storageBucket: "smartzmultipage.appspot.com",
+  messagingSenderId: "398309169877",
+  appId: "1:398309169877:web:daffc48ea2dbe54241a05c",
+  measurementId: "G-746DK30RKC",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 // Reference messages collection
-var messagesRef = firebase.database().ref('scores');
+var scoresRef = firebase.database().ref("scores/");
 
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = firebase.database('mostRecentScore');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+console.log(mostRecentScore);
 
 const MAX_HIGH_SCORES = 10;
 
 saveHighScore = (e) => {
-    e.preventDefault(); //stops the defult from submitting to a new page
+  e.preventDefault(); //stops the defult from submitting to a new page
+  console.log("SAVE SCORE");
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
 
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score); //will filter scores and only display highest numbers
-    highScores.splice(10); //number of highscores to display. any over the top five will be cut off so only top scores are shown
+  scoresRef.set(score);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores)); //updates the highscores and saves them in a string
-    window.location.assign('index.html'); //automatically goes back home after saving
+  /*
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score); //will filter scores and only display highest numbers
+  highScores.splice(10); //number of highscores to display. any over the top five will be cut off so only top scores are shown
+
+  localStorage.setItem("highScores", JSON.stringify(highScores)); //updates the highscores and saves them in a string
+  window.location.assign("index.html"); //automatically goes back home after saving */
 };
 
 /*local storage means you can only use key value pairs with the value being a string so anything being stored will be stored as a string*/
 
-
-finalScore.innerText = mostRecentScore;// NOTE: displays most recent score in html
+finalScore.innerText = mostRecentScore; // NOTE: displays most recent score in html
 
 // Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebase.storage();
+//var storage = firebase.storage();
 // Create a storage reference from our storage service
-var storageRef = storage.ref();
+//var storageRef = storage.ref();
 
-
-
-$('#mathsEnd').submit(function(e) {
+$("#mathsEnd").submit(function (e) {
   e.preventDefault();
 
   var newMessageRef = messagesRef.push();
   newMessageRef.set({
-      name: $('.username').val(),
-      score: $('.score').val(),
+    name: $(".username").val(),
+    score: $(".score").val(),
   });
 
-  $('.success-message').show();
-
-  $('#contactForm')[0].reset();
 });
+
+enableSave = () => {
+  document.getElementById("saveScoreBtn").removeAttribute("disabled");
+};
+document.getElementById("username").addEventListener("keyup", enableSave);
--- gameTest3/javascript/leaderboard.js
+++ gameTest3/javascript/leaderboard.js
@@ -1,14 +1,69 @@
-// NOTE: this shows the data in console
-messagesRef.once('value').then((snapshot) => {
-  Object.keys(snapshot.val()).forEach((key) => {
-      console.log(`Name: ${snapshot.val()[key].name}`);
-      console.log(`Score: ${snapshot.val()[key].score}`);
-  });
-});
+/*Help from https://youtu.be/O4N7yfaJYhI to make this*/
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDuejSqZSWsXGYcO1qaILbyIpJJb5jvIUs",
+  authDomain: "smartzmultipage.firebaseapp.com",
+  databaseURL: "https://smartzmultipage-default-rtdb.firebaseio.com/",
+  projectId: "smartzmultipage",
+  storageBucket: "smartzmultipage.appspot.com",
+  messagingSenderId: "398309169877",
+  appId: "1:398309169877:web:daffc48ea2dbe54241a05c",
+  measurementId: "G-746DK30RKC",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+// Reference messages collection
+var scoresRef = firebase.database().ref("scores/");
+
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+console.log(mostRecentScore);
+
+const MAX_HIGH_SCORES = 10;
 
 
-// NOTE: this is meant to show data in html but its not printing the score
-messagesRef.on("child_added", function(data){
-  childScore = data.val();
-  document.getElementById("scores").innerHTML += childScore.name + "-" + childScore + "<br>";
-});
+function addItemsToList(name, score){
+  var ul=document.getElementsById('highScoresList');
+  var header= document.createElement('h2');
+
+  var header= document.createElement('li'); //the number of highscores shown
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+  var header= document.createElement('li');
+
+  header.innerHTML= 'Highscores-'+ (++scores)
+
+  _name.innerHTML='Name: '+name;
+  _score.innerHTML='Score: '+score;
+
+  ul.appendChild(header);
+  ul.appendChild(_name);
+  ul.appendChild(_score);
+}
+
+
+//calls for the data from database
+function FetchAllData(){
+  firebase.database().ref('scores').once('value', function(snapshot){//a snapshot is a picture of the data at a particular database reference at a single point in time
+    snapshot.forEach(
+      function(ChildSnapshot){
+        let name= ChildSnapshot.val().name;
+        let score= ChildSnapshot.val().score;
+        addItemsToList(name,score);
+      }
+    )
+  })
+}
+
+window.onload(FetchAllData());
